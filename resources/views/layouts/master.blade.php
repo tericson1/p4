9b08702eb6bbc42cbb44668d9a98f2086d7d0057
Tracy Ericson <tracylericson@gmail.com>
2017-12-06 23:40:19 -0500

    adding views

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+        @yield( 'Bill Calculator Tool')
+				Bill Calculator
+    </title>
+
+	<meta charset='utf-8'>
+
+
+<link href="/css/p3.css" type='text/css' rel='stylesheet'>
+
+
+    @stack('head')
+
+</head>
+<body>
+
+	<header>
+		 <h1> Money Forecasting Tool </h1>
+
+	</header>
+
+	<section>
+		@yield('content')
+	</section>
+
+	<footer>
+		<h2><a href='/'>Balances</h2>
+			<h2><a href='/past'>Past Data </h2>
+	</footer>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    @stack('body')
+
+</body>
+</html>
