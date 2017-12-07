2f563af05de9e24c568ee5c13e02eb246d590a25
Tracy Ericson <tracylericson@gmail.com>
2017-12-06 23:44:57 -0500

    added past view

---
+++ resources/views/past.blade.php
@@ -0,0 +1,48 @@
+
+@extends('layouts.master')
+
+@push('head')
+    <link href='/css/p4.css' rel='stylesheet'>
+
+@endpush
+
+@section('content')
+
+<div class="row">
+  <div class="column2" >
+
+<h2>Past Checking Amounts</h2>
+
+<br>
+<br>
+
+</div>
+<div class="column2">
+<h2>Past Bills Listing</h2>
+<br>
+<br>
+<br>
+<br>
+
+</div>
+<div class="column2">
+<h2>Past Income</h2>
+<br>
+<br>
+<br>
+</div>
+</div>
+
+
+
+
+
+</div>
+</div>
+
+
+
+
+
+
+@endsection
